8d456c703a581bef344c867c15f36fdccf504724
Kaiyi Chen <kaiyichen@wustl.edu>
2016-02-08 23:02:33 -0600

    1

---
+++ java_test/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+	public static void main(String args[])
+	{
+		String hello = "HelloWorld";
+
+		for(int i = 0; i < hello.length(); i++)
+		System.out.println(hello.charAt(i));
+	}
+}
